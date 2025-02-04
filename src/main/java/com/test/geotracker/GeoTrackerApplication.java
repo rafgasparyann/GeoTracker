fcc78de4f4bd487e71b396fdfb459d3c437891d8
Raf <raf.gasparyan.17@mail.ru>
2025-02-04 21:14:56 +0400

    Initial commit

---
+++ src/main/java/com/test/geotracker/GeoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.test.geotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/geotracker/GeoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.geotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
